0a13f5049176d751216ab135a9f7ad72b66b40f0
Nam Pham <nampham@brodev.com>
2017-09-19 23:30:57 +0700

    test

---
+++ src/views/index.jsx
@@ -0,0 +1,11 @@
+import ReactDom from 'react-dom';
+import React from 'react';
+
+const el = (
+  <p>Yes</p>
+);
+
+ReactDom.render(
+  el,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const glob = require('glob');
+const PurifyCSSPlugin = require('purifycss-webpack');
+const prodEnv = (process.env.NODE_ENV === 'production');
+
+module.exports = {
+    entry: {
+        app: [
+            './src/views/index.jsx',
+            './src/assets/style/style.scss'
+        ]
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    use: ['css-loader', 'sass-loader'],
+                    fallback: 'style-loader'
+                })
+            },
+            {
+                test: /\.(png|jpeg|jpg|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'images/[name].[ext]'
+                        }
+                    },
+                    'img-loader'
+                ]
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015', 'react'],
+                        plugins: ['transform-runtime']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style/[name].css'),
+        // new PurifyCSSPlugin({
+        //     paths: glob.sync(path.join(__dirname, '../index.html')),
+        //     minimize: prodEnv
+        // })
+    ],
+    watch: true
+};
+
+if (prodEnv) {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin()
+    );
+}
